df08706049456a6728048b302447df42b6569558
rajat <rajat.kumar@neoito.com>
2021-03-09 23:48:44 +0530

    added question on day3

--- day3/exercises/Solutions.js
+++ day3/exercises/Solutions.js
@@ -1,9 +1,23 @@
 /**
- * Find the fruit name whose first character is m
+ * Find the  first fruit name whose first character is m
  * author: https://github.com/rajat-ds
  * date: 09-03-21
  */
 
-const trees = ["apple", "banana", "tomato", "mango","cherry"];
+const fruits = ["apple", "banana", "tomato", "mango", "cherry"];
+const fruitResult = fruits.find((fruit) => fruit.startsWith("m"));
+console.log("fruit starting with m ", fruitResult);
 
-const result = trees.find((tree) => tree.startsWith("m"));
+/**
+ * Find the  first tree having name oak
+ * author: https://github.com/rajat-ds
+ * date: 09-03-21
+ */
+const trees = [
+  { name: "birch", count: 4 },
+  { name: "maple", count: 5 },
+  { name: "oak", count: 2 },
+];
+
+const result = trees.find((tree) => tree.name === "oak");
+console.log(result)
