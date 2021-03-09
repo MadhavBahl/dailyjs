33e5e9249604bd9fedd67ffb115483a8cc903da7
rajat <rajat.kumar@neoito.com>
2021-03-09 23:41:48 +0530

    added question and solution for day3

---
+++ day3/exercises/Solutions.js
@@ -0,0 +1,9 @@
+/**
+ * Find the fruit name whose first character is m
+ * author: https://github.com/rajat-ds
+ * date: 09-03-21
+ */
+
+const trees = ["apple", "banana", "tomato", "mango","cherry"];
+
+const result = trees.find((tree) => tree.startsWith("m"));
